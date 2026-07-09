24327ae4e0d1a6786f65cfa0db51f3006e78cb11
Sippos <63863989+Sippos@users.noreply.github.com>
2026-07-09 17:18:34 +0200

    Add StompSprite icon to level up menu

--- level_up_menu.gd
+++ level_up_menu.gd
@@ -2,6 +2,15 @@ extends CanvasLayer
 
 signal upgrade_selected(upgrade_type: String)
 
+const STOMP_SPRITE_TEXTURES = [
+	"res://StompSprite.png",
+	"res://StompSprite.webp",
+	"res://StompSprite.tres",
+	"res://stomp_sprite.png",
+	"res://sprites/StompSprite.png",
+	"res://assets/StompSprite.png"
+]
+
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS # keep running while tree paused
 
@@ -11,7 +20,25 @@ func setup(has_stomp: bool) -> void:
 		btn_stomp.text = "Upgrade Stomp (Level Up)"
 	else:
 		btn_stomp.text = "Learn Stomp"
+	_apply_stomp_sprite_to_button(btn_stomp)
 	btn_stomp.call_deferred("grab_focus")
+
+func _load_stomp_texture() -> Texture2D:
+	for texture_path in STOMP_SPRITE_TEXTURES:
+		if ResourceLoader.exists(texture_path):
+			var texture = load(texture_path)
+			if texture is Texture2D:
+				return texture
+	return null
+
+func _apply_stomp_sprite_to_button(button: Button) -> void:
+	var texture = _load_stomp_texture()
+	if texture == null:
+		return
+	button.icon = texture
+	button.expand_icon = true
+	button.icon_alignment = HORIZONTAL_ALIGNMENT_LEFT
+	button.add_theme_constant_override("icon_spacing", 12)
 
 func _on_button_stomp_pressed() -> void:
 	upgrade_selected.emit("stomp")
