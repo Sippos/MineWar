60637e57012d1ebd9f0cb2bc49e710a706314a21
Sippos <63863989+Sippos@users.noreply.github.com>
2026-07-10 11:39:23 +0200

    Use compact complete hero ability bootstrap

--- hero_ability_bootstrap.gd
+++ hero_ability_bootstrap.gd
@@ -1,17 +1,21 @@
 extends Node
 
 const HERO_ABILITIES_SCRIPT = preload("res://hero_abilities.gd")
-const PLAYER_SCRIPT_PATH := "res://player.gd"
 
 func _ready() -> void:
-	get_tree().node_added.connect(_on_node_added)
-	call_deferred("_scan_existing_nodes")
+	get_tree().node_added.connect(_try_attach)
+	call_deferred("_scan_players")
 
-func _scan_existing_nodes() -> void:
-	_scan_branch(get_tree().root)
+func _scan_players() -> void:
+	for node in get_tree().root.find_children("Player", "CharacterBody2D", true, false):
+		_try_attach(node)
 
-func _scan_branch(node: Node) -> void:
-	if not is_instance_valid(node):
+func _try_attach(node: Node) -> void:
+	if not is_instance_valid(node) or node.name != "Player" or not (node is CharacterBody2D):
 		return
-	_try_attach(node)
-	for child in node.get
+	if node.get_node_or_null("HeroAbilities"):
+		return
+	var controller := Node.new()
+	controller.name = "HeroAbilities"
+	controller.set_script(HERO_ABILITIES_SCRIPT)
+	node.add_child(controller)
