9b43deda84465286bb7fd655c5343f5ce597a289
Sippos <63863989+Sippos@users.noreply.github.com>
2026-07-10 11:38:39 +0200

    Restore hero ability bootstrap attachment

--- hero_ability_bootstrap.gd
+++ hero_ability_bootstrap.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const HERO_ABILITIES_SCRIPT = preload("res://hero_abilities.gd")
+const PLAYER_SCRIPT_PATH := "res://player.gd"
 
 func _ready() -> void:
 	get_tree().node_added.connect(_on_node_added)
@@ -10,9 +11,7 @@ func _scan_existing_nodes() -> void:
 	_scan_branch(get_tree().root)
 
 func _scan_branch(node: Node) -> void:
+	if not is_instance_valid(node):
+		return
 	_try_attach(node)
-	for child in node.get_children():
-		_scan_branch(child)
-
-func _on_node_added(node: Node) -> void:
-	call_de
+	for child in node.get
